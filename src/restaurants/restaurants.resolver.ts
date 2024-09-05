f639a601601694395a65ce79297f38c2a393ebad
zestlee1106 <zestlee1106@gmail.com>
2024-09-05 21:23:42 +0900

    add: restaurant entity 추가

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,10 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver()
+@Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query(() => Boolean)
-  isPizzaGood() {
+  @Query((returns) => Restaurant)
+  myRestaurant() {
     return true;
   }
 }
