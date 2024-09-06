02b225d32f6670fc3459572d3d6edd5107165a13
zestlee1106 <zestlee1106@gmail.com>
2024-09-06 21:07:31 +0900

    add: mutation 추가 및 DTO 추가
    - gql 에서는 요청할 인자를 @Args 데코레이터로 요청함
    - mutation 에서 넣어줄 값을 dto 로 정의할 수가 있는데, InputType, ArgsType 이 있음
    - InputType 은 넣어줄 때 객체로 묶어서 보내야 하고, ArgsType 은 각각 넣어줄 수가 있음

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,10 +1,17 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query((returns) => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
+  }
+
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto) {
+    return createRestaurantDto;
   }
 }
