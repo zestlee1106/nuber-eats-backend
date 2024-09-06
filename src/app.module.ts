a60061891770ebce6e194ad8a2c02b2898ce7d01
zestlee1106 <zestlee1106@gmail.com>
2024-09-06 21:40:31 +0900

    add: typeorm module 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,25 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'ywlee',
+      password: '123', // 로컬 호스트에서는 막 적어도 됨
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
