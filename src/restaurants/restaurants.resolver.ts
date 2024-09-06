a88066402b83d061aa5353e1d2be06c7611c4414
zestlee1106 <zestlee1106@gmail.com>
2024-09-06 22:44:50 +0900

    update: Restaurant 서비스 추가
    - restaurant 서비스를 추가한다
    - 서비스 추가를 위해 모듈에서 Restaurant 를 typorm 의 feature 로 import
    - 리졸버에서 사용하기 위해 모듈에서 서비스를 provide 한다
    - 리졸버에서 서비스를 주입하고 함수 내부에서 서비스의 내부 함수를 사용한다

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,13 +1,15 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+
   @Query((returns) => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log(veganOnly);
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
 
   @Mutation((returns) => Boolean)
