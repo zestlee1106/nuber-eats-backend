cfd95b2e52d897069089b2763ae79ae1d3ff1c32
zestlee1106 <zestlee1106@gmail.com>
2024-09-06 23:00:03 +0900

    update: createRestaurant 추가

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Restaurant } from './entities/restaurant.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 
 @Injectable()
 export class RestaurantService {
@@ -12,5 +13,12 @@ export class RestaurantService {
 
   getAll(): Promise<Restaurant[]> {
     return this.restaurants.find();
+  }
+
+  createRestaurant(
+    createRestaurantDto: CreateRestaurantDto,
+  ): Promise<Restaurant> {
+    const newRestaurant = this.restaurants.create(createRestaurantDto);
+    return this.restaurants.save(newRestaurant);
   }
 }
